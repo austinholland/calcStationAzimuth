a18994598ba01e44c0d9ecad0e1717ecd04b2719
Holland, Austin Adams <aaholland@usgs.gov>
2018-02-09 11:10:33 -0700

    Added plotting functionality

--- azimuth_dirver.py
+++ azimuth_dirver.py
@@ -2,22 +2,10 @@
 gatAzi.py on github kschramm-usgs
 """
 import os
-
-fh=open('query.csv')
-header=fh.readline()
-resdirs=[]
-for line in fh:
-  lvals=line.split(',')
-  ot=lvals[0]
-  # Split the time to make result directories
-  tsplit=ot.split('T')
-  dir=tsplit[0]
-
-  resdirs.append(dir)
-  cmd="./getAzi.py -resDir %s -n US -cha 'LH*' -eventTime '%s' -eventLat %f -eventLon %f -eventDepth %d -sta '*'" % (dir,ot,float(lvals[1]),float(lvals[2]),float(lvals[3]))
-  print(cmd)
-  os.system(cmd)
-fh.close()
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime.datetime as datetime
+from obspy.core import *
 
 def add_azi_measurment(stadict,stakey,time,difference,linearity):
   """ This method is a hack to allow evaluating stations using multiple events and somewhat
@@ -37,6 +25,46 @@ def plot_network_azi(stadict):
   """ This method takes a dictionary of azimuth measurements for multiple events and 
   creates plots for station netwok location NSL keys.
   """
+  for keys in stadict.keys():
+  
+  
+def process_results(stadict,dir,ot):
+  """ Read in data from the results for each event and add it to the station dictionary
+  """
   # If the absolute value of linearity 
   ignore_linearity=0.87
-  for keys in stadict.keys():
+  # Process our output
+  csvfile="%s/Results.csv" % (dir)
+  csv=open(csvfile,'r')
+  hl=csv.readline() # Read the header
+  for line in csv:
+    lvals=line.split(',')
+    nsl="%s-%s-%s" % (lvals[0],lvals[1],lvals[2])
+    difference=float(lvals[3])-float(lvals[4])
+    linearity=np.abs(float(lvals[6])
+    if linearity>=ignore_linearity:  #Could move this to the plot section but it adds complexity if you do
+      stadict=add_azi_measurement(stadict,nsl,ot,difference,linearity)
+  return stadict
+
+
+if __name__=="__main__":
+  stadict={}
+  fh=open('query.csv')
+  header=fh.readline()
+  resdirs=[]
+  for line in fh:
+    lvals=line.split(',')
+    ot=lvals[0]
+    # Split the time to make result directories
+    tsplit=ot.split(':')
+    dir=tsplit[0]
+    ot=UTCDateTime(ot)
+
+    resdirs.append(dir)
+    cmd="./getAzi.py -resDir %s -n US -cha 'LH*' -eventTime '%s' -eventLat %f -eventLon %f -eventDepth %d -sta '*'" % (dir,ot,float(lvals[1]),float(lvals[2]),float(lvals[3]))
+    print(cmd)
+    os.system(cmd)
+    stadict=process_results(stadict,dir,ot.datetime)
+    
+  fh.close()
+
