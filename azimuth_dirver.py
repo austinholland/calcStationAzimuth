2351fbed1530284f121c43e26809ed6ca7b4d70a
Holland, Austin Adams <aaholland@usgs.gov>
2018-02-09 08:59:33 -0700

    Add support for multiple event plotting

--- azimuth_dirver.py
+++ azimuth_dirver.py
@@ -18,3 +18,25 @@ for line in fh:
   print(cmd)
   os.system(cmd)
 fh.close()
+
+def add_azi_measurment(stadict,stakey,time,difference,linearity):
+  """ This method is a hack to allow evaluating stations using multiple events and somewhat
+  breaks the original concept for this program.  Ideally this program would be built more
+  modular such that another script could access functions here, but since that is not
+  the case we will use this hack.
+  This solution uses a dictionary with the NSL (Station,Network,Location) as the
+  key it then saves the data in a nested list.
+  """
+  if stakey in stadict.keys():
+      stadict[stakey].append([time,difference,linearity])
+  else:
+      stadict[stakey]=[[time,difference,linearity]]
+  return stadict
+  
+def plot_network_azi(stadict):
+  """ This method takes a dictionary of azimuth measurements for multiple events and 
+  creates plots for station netwok location NSL keys.
+  """
+  # If the absolute value of linearity 
+  ignore_linearity=0.87
+  for keys in stadict.keys():
